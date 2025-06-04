10aea55efa565a36f6dac32d6ef06ddb6afd3f61
KALYANISHARNAGATCS <sharnagatkalyani0812@gmail.com>
2025-06-04 16:39:10 +0530

    my next-js projects

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,61 @@
+// import Image from "next/image";
+// import styles from "./page.module.css";
+"use client";
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
+import "./globals.css";
+
+
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [domain, setDomain] = useState("");
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    setDomain(window.location.hostname);
+  }, []);
+
+  return (
+    <section className={styles.wrapper}>
+      <div className={styles.boxWrapper}>
+        <div className={styles.leftContainer}>
+          <div className={styles.content}>
+            <p className={styles.title}>
+              www.<span className={styles.domainText}>{domain}</span> <br />Under Maintenance.
+            </p>
+            <p className={styles.description}>
+              We apologize for any inconvenience caused, but our website is currently undergoing scheduled
+              maintenance to enhance your browsing experience.
+            </p>
+          </div>
+          <div className={styles.contactInfo}>
+            <div className={styles.email}>
+              <strong>Email</strong> <br />
+              <a href="mailto:hello@rsinfotechsys.com">
+                hello@rsinfotechsys.com
+              </a>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+        <div className={styles.rightContainer}>
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/image copy 2.png"
+            alt="under maintenance"
+            draggable={false}
+            width={500}
+            height={500}
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </div>
+      </div>
+
+      <div className={styles.footer}>
+        <p>
+          © 2023 All Rights Reserved by <span className={styles.domainText}>{domain}</span> &nbsp; Designed & Developed By
+          <a href="https://rsinfotechsys.com"> Right Serve Infotech System Pvt. Ltd.</a>
+        </p>
+      </div>
+    </section>
   );
 }
